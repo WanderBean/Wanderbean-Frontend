6bab8c325bdaa7719118028f6e954b18a3ad2670
Simona Wurst <simona_wu@yahoo.de>
2024-03-08 11:26:58 +0100

    Add Cafedetailspage basic info according to Cafe Model

--- src/pages/CafeDetailsPage.jsx
+++ src/pages/CafeDetailsPage.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react"
 import { useNavigate, useParams } from "react-router-dom"
+import axios from "axios"
 
 
 function CafeDetailsPage() {
@@ -14,6 +15,7 @@ function CafeDetailsPage() {
     axios.get(`${API_URL}/cafes/${id}`)
       .then((response) => {
         setCafe(response.data)
+        console.log(response.data)
       })
       .catch((err) => {
         setErrorMessage(err.response.data.message)
@@ -26,9 +28,32 @@ function CafeDetailsPage() {
 
 
   return (
-    <>
-      <h1>I am a Cafe Details Page</h1>
-    </>
+    <div>
+      {cafe === null
+        ? <p>Loading...</p>
+        : (
+          <>
+            <div className="DetailPageContainer">
+              <div>
+                <img className="CafeDetailImg" src={cafe.image} alt={cafe.title} />
+              </div>
+              <div>
+                <h1>{cafe.title}</h1>
+                <p>{cafe.description}</p>
+                {cafe.location && cafe.location.length > 0 && (
+                  <div>
+                    <p>{cafe.location[0].city}, {cafe.location[0].neighborhood}</p>
+                    <p>{cafe.location[0].adress}</p>                   {/* Check again as Schema was changed */}
+                  </div>
+                )}
+                <div>{Array.isArray(cafe.specs) && cafe.specs.map((specs, index) => (
+                  <label key={index}>{specs}</label>
+                ))}</div>
+              </div>
+            </div>
+          </>
+        )}
+    </div>
   );
 }
 
