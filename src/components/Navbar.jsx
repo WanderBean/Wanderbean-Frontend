b3417cfdbdbb4d124583694ac5dbb57519cc607d
Simona Wurst <simona_wu@yahoo.de>
2024-03-08 15:32:41 +0100

    Add Logout (remove Token when clicking on logout button); toggle between login/logout button depending if there is a token in the localStorage

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,31 @@
-import React from "react"
+import React, { useState, useContext } from "react"
+import { AuthContext } from "../context/auth.context"
 import { Link } from "react-router-dom"
 import logo from "../img/coffee-beans.png"
 
 function NavBar() {
+
+    {/* Login & Logout functionality - toggle between buttons*/}
+
+    const { storeToken, authenticateUser } = useContext(AuthContext);
+    const storedToken = localStorage.getItem("authToken")
+
+    const removeToken = () => {
+        localStorage.removeItem("authToken")
+    }
+    
+    const isLoggedIn = storedToken !== null
+
     return (
         <div className="NavBar">
             <Link to="/"><img src={logo} alt="Wanderbeans Logo" /></Link>
             <Link to="/cafes">Find a Cafe</Link>
             <Link to="/signup">Signup</Link>
-            <Link to="/login">Login</Link>
+
+            {isLoggedIn === false 
+            ? (<Link to="/login">Login</Link>)
+            : (<Link onClick={removeToken}>Logout</Link>)
+            }
         </div>
     )
 }
