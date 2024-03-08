c1e233675022904d94a3efa6dd1c4fd444f7b807
arisar86 <ari.sariannidis@gmail.com>
2024-03-08 14:33:49 +0100

    put cafeAdd and cafeList Functions in their own components

--- src/pages/CafeListPage.jsx
+++ src/pages/CafeListPage.jsx
@@ -1,52 +1,9 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import Searchbar from "../components/Searchbar";
+import CafeList from "../components/CafeList";
 
 function CafeListPage() {
-  const database = "http://localhost:5005";
-  const [allCafes, setAllCafes] = useState([]);
-  const [searchTerm, setSearchTerm] = useState("");
-
-  const getAllCafes = () => {
-    axios
-      .get(`${database}/cafes`)
-      .then((response) => {
-        setAllCafes(response.data);
-      })
-      .catch((error) => {
-        console.log("Error getting Cafes from the API...");
-        console.log(error);
-      });
-  };
-
-  useEffect(() => {
-    getAllCafes();
-  }, []);
-
-  const handleSearch = (event) => {
-    const searchValue = event.target.value;
-    setSearchTerm(searchValue);
-  };
-
-  const filteredCafes = allCafes.filter((cafe) =>
-    cafe.title.toLowerCase().includes(searchTerm.toLowerCase())
-  );
-
   return (
     <>
-      <Searchbar searchTerm={searchTerm} handleSearch={handleSearch} />
-      <div>
-        {filteredCafes.map((cafe) => (
-          <div key={cafe._id}>
-            <Link to={`/cafes/${cafe._id}`}>
-              <img src={cafe.image} alt={cafe.title} />
-              <p>{cafe.title}</p>
-              <p>{cafe.location[0].city}</p>
-            </Link>
-          </div>
-        ))}
-      </div>
+      <CafeList />
     </>
   );
 }
