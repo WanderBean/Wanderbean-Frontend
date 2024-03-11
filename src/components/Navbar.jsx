b9b41f26d31b2352f754c82449134134fa3b2549
Simona Wurst <simona_wu@yahoo.de>
2024-03-11 14:31:59 +0100

    Update NavBar - display user.name when logged in

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,22 +5,25 @@ import logo from "../img/coffee-beans.png"
 
 function NavBar() {
 
-    {/* Login & Logout functionality - toggle between buttons depending if there is a token in localStorage*/}
-
     const { user, isLoggedIn, logOutUser } = useContext(AuthContext);
 
     return (
         <div className="NavBar">
             <Link to="/"><img src={logo} alt="Wanderbeans Logo" /></Link>
             <Link to="/cafes">Find a Cafe</Link>
-            <Link to="/signup">Signup</Link>
-            
 
-            {isLoggedIn === false 
-            ? 
-            (<Link to="/login">Login</Link>)
-            : 
-            (<Link onClick={logOutUser}>Logout</Link>)
+            {/* Login & Logout functionality - toggle between buttons*/}
+            {isLoggedIn === false
+                ?
+                <>
+                    <Link to="/signup">Signup</Link>
+                    <Link to="/login">Login</Link>
+                </>
+                :
+                <>
+                    <p>Hi, {user.name}</p>
+                    <Link onClick={logOutUser}>Logout</Link>
+                </>
             }
         </div>
     )
