89b17bc4f0c1c56ba999c68728192d4a49c773dc
Simona Wurst <simona_wu@yahoo.de>
2024-03-11 15:31:15 +0100

    Initial setup Tailwind CSS

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function HomePage () {
     return (
         <>
-        <h1>I am a Homepage</h1>
+        <h1  className="text-5xl font-bold underline">I am a Homepage</h1>
         </>
     )
 }
