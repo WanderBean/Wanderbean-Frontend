807d334f4467626ed88e670a9b222ee20dba621a
Simona Wurst <simona_wu@yahoo.de>
2024-03-12 09:18:05 +0100

    404 page initial setup

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,9 +1,17 @@
-import React from "react"; 
+import React from "react";
+import { Link } from "react-router-dom";
 
-function NotFoundPage () {
+function NotFoundPage() {
     return (
         <>
-        <h1>404 - Not Found</h1>
+            <div>
+                <h1 className="text-5xl p-10 flex justify-center">
+                    Meow, we ran out of coffeine!</h1>
+                <p className="text-3xl p-10 flex justify-center">Please make your way back and discover another beautiful café.</p>
+                <Link to="/cafes" className="flex justify-center bg-black hover:bg-gray-700 text-white font-semibold py-2 px-4 border border-black-200 shadow">
+                    Explore all Cafés
+                </Link>
+            </div>
         </>
     )
 }
