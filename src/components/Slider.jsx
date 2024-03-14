7048f35645970547281aa675f23ad2cf339e7cea
arisar86 <ari.sariannidis@gmail.com>
2024-03-14 11:46:29 +0100

    links for slides done

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,13 +1,23 @@
 import { useEffect, useState } from "react";
 import { GrNext, GrPrevious } from "react-icons/gr";
 import logo from "../img/coffee-beans-white.png";
+import { Link } from "react-router-dom";
 
 const featuredCafes = [
-  "src/img/Coffee_Collective_Copenhagen.jpg",
-  "src/img/Coffee_Fuglen_Tokyo.jpg",
-  "src/img/The_Barn_Berlin.jpg",
-  "src/img/Topos_Bookstore_Cafe_NewYork.jpg",
-  "src/img/Workshop_Coffee_Co._London.jpg",
+  {
+    id: "65f2b7931ac3ed6d4564eb76",
+    image: "src/img/Coffee_Collective_Copenhagen.jpg",
+  },
+  { id: "65f2cdce1ac3ed6d4564ec91", image: "src/img/Coffee_Fuglen_Tokyo.jpg" },
+  { id: "65eee387390cd13035bd12ca", image: "src/img/The_Barn_Berlin.jpg" },
+  {
+    id: "65f2d01f1ac3ed6d4564ecbd",
+    image: "src/img/Topos_Bookstore_Cafe_NewYork.jpg",
+  },
+  {
+    id: "65f2cf451ac3ed6d4564eca6",
+    image: "src/img/Workshop_Coffee_Co._London.jpg",
+  },
 ];
 
 function Slider() {
@@ -36,11 +46,13 @@ function Slider() {
   return (
     <div className="w-full select-none relative aspect-w-16 aspect-h-9">
       <div className="w-full h-full relative">
-        <img
-          src={featuredCafes[currentIndex]}
-          alt={`Cafe ${currentIndex + 1}`}
-          className="w-full h-full object-cover"
-        />
+        <Link to={`/cafes/${featuredCafes[currentIndex].id}`}>
+          <img
+            src={featuredCafes[currentIndex].image}
+            alt={`Cafe ${currentIndex + 1}`}
+            className="w-full h-full object-cover"
+          />
+        </Link>
       </div>
 
       <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex items-center border-solid border-4 border-white m-1 px-4 py-2">
